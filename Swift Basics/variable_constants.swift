b51158105005fc5e5377265cb91ba1d954d5ce3b
Hackveda <admin@hackveda.in>
2021-09-22 12:37:46 +0530

    Lesson 8

---
+++ Swift Basics/variable_constants.swift
@@ -0,0 +1,22 @@
+// Printing something on the console using Swift
+print("Hello World")
+
+// Using the constants and variables in Swift
+// Example: Max Heart Rate Calculator in Swift
+// MaxHeartrate = 220 - Age
+
+// Declare a constant using let
+let valueConstant = 220
+print("The value of contant is: ")
+print(valueConstant)
+
+// Declare the variables using var keyword
+var age = 31
+var maxheartrate = 0
+
+// Processing using constant and variables 
+maxheartrate = valueConstant - age
+
+// Output of the program
+print("Max Heart Rate of the user is: ")
+print(maxheartrate)
