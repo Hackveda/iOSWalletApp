f159234c5cad313727d4674a52d8425c32cf56d0
Hackveda <admin@hackveda.in>
2021-09-22 12:45:23 +0530

    Lesson 8

---
+++ Swift Basics/type_annotations_comments.swift
@@ -0,0 +1,23 @@
+// Type Annotations
+
+let valueConstant: Int
+let maxheartrate: Int
+let age: Int
+
+/*
+valueConstant = 220
+age = 31
+maxheartrate = valueConstant - age
+print(maxheartrate)
+*/
+
+// Define String Type Annotations
+
+var username: String
+username = "hackveda"
+print(username)
+
+// Define Double Type Annotations
+var height: Double
+height = 5.9
+print(height)
